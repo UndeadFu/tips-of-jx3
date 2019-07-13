03cf7f85b994aec175fdb72038bc9edfce490da4
UndeadFu <1015467204@qq.com>
2019-07-13 10:06:32 +0800

    fix something

--- 02collectInfos.py
+++ 02collectInfos.py
@@ -4,11 +4,10 @@ import sqlite3
 import sys
 
 
-def letsgo3():
-	conn = sqlite3.connect("collections.db")
+def letsgo1(conn):
 	c0 = conn.cursor()
 	c1 = conn.cursor()
-	c0.execute("SELECT FULL, FAVOR, ADDON, NAME, POT, MATERIALS from foods where GOOD = \"{}\"".format(sys.argv[1]))
+	c0.execute("SELECT FULL, FAVOR, ADDON, NAME, POT, MATERIALS from foods where GOOD = \"{}\"".format(sys.argv[2]))
 	for row in c0:
 		print("菜名  ：" + row[3])
 		print("饱食度：" + str(row[0]))
@@ -20,13 +19,41 @@ def letsgo3():
 			print("\t", mat)
 			for row2 in c1:
 				print("\t\t", row2[0], row2[1])
+def letsgo2(conn):
+	c0 = conn.cursor()
+	c0.execute("SELECT LOCATION, ADDRESS, THING from collection_infos where QUALITY = \"{}\" ORDER BY THING".format(sys.argv[2]))
+	thing = None
+	for row in c0:
+		if thing != row[2]:
+			thing = row[2]
+			print(thing)
+		print("\t" + row[0] + " "+ row[1])
+
+def letsgo3(conn):
+	c0 = conn.cursor()
+	c0.execute("SELECT LOCATION, ADDRESS from collection_infos where THING = \"{}\"".format(sys.argv[2]))
+	for row in c0:
+		print(row[0] + " " + row[1])
+
+
+import os
+def main():
+	the_path = os.path.dirname(sys.argv[0])
+	os.chdir(the_path)
+	if(len(sys.argv)!= 3):
+		print("example:\n\tpython ...py 人物 渡会 ---- 得到该人物绝赞的菜")
+		print("\tpython ...py 品质 紫色 ---- 得到所有紫色物品的采集地点")
+		print("\tpython ...py 物品 佛珠 ---- 得到佛珠的采集地点")
+		return
+	conn = sqlite3.connect("collections.db")
+	if sys.argv[1] == "人物":
+		letsgo1(conn)
+	elif sys.argv[1] == "品质":
+		letsgo2(conn)
+	elif sys.argv[1] == "物品":
+		letsgo3(conn)
 	conn.commit()
 	conn.close()
-def main():
-	if(len(sys.argv)!= 2):
-		print("example:\npython ...py 渡会")
-	letsgo3()
-
 
 if __name__ == '__main__':
 	main()
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # tips-of-jx3 
+
+
+
+01sync.py 是用来将 collections.xls 中的数据同步到数据库 collections.db 中
+
+
+
+02collections.py
+
+example:
+​        python 02collections.py 人物 渡会 ---- 得到该人物绝赞的菜
+​        python 02collections.py 品质 紫色 ---- 得到所有紫色物品的采集地点
+​        python 02collections.py 物品 佛珠 ---- 得到佛珠的采集地点
+
+collections.db 的数据库结构如下：
+
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE collection_infos (ID INTEGER PRIMARY KEY AUTOINCREMENT, LOCATION TEXT NOT NULL, ADDRESS TEXT NOT NULL, THING TEXT NOT NULL, QUALITY TEXT NOT NULL);
+CREATE TABLE foods (ID INTEGER PRIMARY KEY AUTOINCREMENT, FULL INTEGER NOT NULL, FAVOR INTER NOT NULL, POT TEXT NOT NULL, ADDON TEXT, GOOD TEXT, NAME TEXT NOT NULL, MATERIALS TEXT);
+
+
+
